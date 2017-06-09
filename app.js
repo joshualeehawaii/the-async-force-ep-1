176bfa76ca53d4fead98a51dbb5685fc0d51c334
Joshua Lee <jdl93@Joshuas-MacBook-Air.local>
2017-06-08 17:10:24 -1000

    on last set of instructions

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
  (function (){
   console.log('sanity check');
 
+  //This is instruction 6
    var oReq = new XMLHttpRequest();
 
     function getName(){
@@ -23,18 +24,63 @@
 
     function getPlanet(){
       var response = JSON.parse(this.responseText);
-      console.log(this.responseText); //typeof is a string
-      console.log(response); //data parsed into readable text (JSON Object)
-      console.log(response.name);
 
      var name = document.getElementById('person4HomeWorld');
-      name.innerHTML = response.name; //JSON Object back to text
+      name.innerHTML = response.name;
    }
      function findPlanet(id){
       oReq2.addEventListener('load', getPlanet);
-      oReq2.open('GET', `http://swapi.co/api/planets/${id}`); //Darth Vader Homeworld
+      oReq2.open('GET', `http://swapi.co/api/planets/${id}`);
       oReq2.send();
    }
    findPlanet(1);
+
+   //This is instruction 7
+   var oReq3 = new XMLHttpRequest();
+
+    function getNewName(){
+     var response = JSON.parse(this.responseText);
+
+     var name = document.getElementById('person14Name');
+     name.innerHTML = response.name;
+  }
+   function findNewName(id){
+    oReq3.addEventListener('load', getNewName);
+    oReq3.open('GET', `http://www.swapi.co/api/people/${id}`);
+    oReq3.send();
+  }
+  findNewName(14);
+
+  var oReq4 = new XMLHttpRequest();
+
+    function getSpecies(){
+      var response = JSON.parse(this.responseText);
+
+     var name = document.getElementById('person14Species');
+      name.innerHTML = response.name;
+   }
+     function findSpecies(id){
+      oReq4.addEventListener('load', getSpecies);
+      oReq4.open('GET', `http://swapi.co/api/species/${id}`);
+      oReq4.send();
+   }
+   findSpecies(1);
+
+   //This is instruction 8
+
+   var oReq5 = new XMLHttpRequest();
+
+    function getFilms(){
+     var response = JSON.parse(this.responseText);
+
+     var name = document.getElementById('person14Name');
+     name.innerHTML = response.name;
+  }
+   function findFilms(id){
+    oReq5.addEventListener('load', getFilms);
+    oReq5.open('GET', `http://www.swapi.co/api/people/${id}`);
+    oReq5.send();
+  }
+  findFilms(14);
 
  })();
